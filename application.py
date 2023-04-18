e9974b02a15d1c0f87137dae05d469c173ac2090
Arun Joseph <arunjoseph.business@gmail.com>
2023-04-18 13:45:41 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,21 +4,9 @@ from flask import Flask
 from Spacy import *
 
 res = process_spacy("Why talk about what we want? That is childish. Absurd. Of course, you are interested in what you want. You are eternally interested in it. But no one else is. The rest of us are just like you: we are interested in what we want.")
-# print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % res
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Marble</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
 application = Flask(__name__)
 
 # add a rule for the index page.
